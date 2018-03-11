689e1934f506fbe74380b1e3c8b5f162ce94ef13
Francisco Zurita <fzurita@gmail.com>
2018-03-11 11:14:42 -0400

    angrylion-plus: Fix some synchronization issues

--- mupen64plus-video-angrylion-plus/upstream/core/parallel.cpp
+++ mupen64plus-video-angrylion-plus/upstream/core/parallel.cpp
@@ -7,15 +7,6 @@
 #include <mutex>
 #include <condition_variable>
 #include <atomic>
-#include <android/log.h>
-
-void LOG(const char * format, ...) {
-    va_list va;
-    va_start(va, format);
-    __android_log_vprint(ANDROID_LOG_ERROR, "Parallel", format, va);
-    va_end(va);
-}
-
 
 class Parallel
 {
@@ -24,10 +15,16 @@ public:
 
         // give workers an empty task
         m_task = [](uint32_t) {};
+        m_accept_work = true;
+        m_num_workers = num_workers;
+
+        // set work available status
+        for (uint32_t worker_id = 0; worker_id < num_workers-1; worker_id++) {
+            m_work_available.emplace_back(true);
+        }
 
         // create worker threads
-        for (uint32_t worker_id = 1; worker_id < num_workers; worker_id++) {
-            m_work_available.emplace_back(false);
+        for (uint32_t worker_id = 0; worker_id < num_workers-1; worker_id++) {
             m_workers.emplace_back(std::thread(&Parallel::do_work, this, worker_id));
         }
 
@@ -63,30 +60,30 @@ public:
 
         // prepare task for workers and send signal so they start working
         m_task = task;
-        for (auto&& workAvailable : m_work_available) {
-            workAvailable = true;
+
+        {
+            std::unique_lock<std::mutex> ul(m_signal_mutex);
+            for (auto&& workAvailable : m_work_available) {
+                workAvailable = true;
+            }
         }
 
-
-		LOG("WORK START");
         m_signal_work.notify_all();
-		m_task(0);
+        m_task(m_num_workers-1);
 
         // wait for all workers to finish
         wait();
-		LOG("WORK END");
     }
 
 private:
     std::function<void(uint32_t)> m_task;
     std::vector<std::thread> m_workers;
-    std::mutex m_done_mutex;
-    std::mutex m_work_mutex;
     std::mutex m_signal_mutex;
     std::condition_variable m_signal_work;
     std::condition_variable m_signal_done;
     std::vector<bool> m_work_available;
-    std::atomic_bool m_accept_work{true};
+    bool m_accept_work;
+    uint32_t m_num_workers;
 
     void do_work(int32_t worker_id) {
 
@@ -96,47 +93,30 @@ private:
             if (m_accept_work) {
                 m_task(worker_id);
             }
+            
+            std::unique_lock<std::mutex> ul(m_signal_mutex);
+            
+            // task is done, update number of active workers
+            // and signal main thread
+            m_work_available[worker_id] = false;
 
-            {
-                std::unique_lock<std::mutex> ul(m_signal_mutex);
-
-                // task is done, update number of active workers
-                // and signal main thread
-                m_work_available[worker_id-1] = false;
-
-                bool readyToSignal = true;
-                for (int index = 0; index < m_work_available.size() && readyToSignal; ++index) {
-                    readyToSignal = !m_work_available[index];
-                }
-
-                if(readyToSignal) {
-                    LOG("%d: ABOUT TO SIGNAL", worker_id);
-                    m_signal_done.notify_one();
-                    LOG("%d: SIGNALED", worker_id);
-                }
-            }
-
+            m_signal_done.notify_one();
+        
             // go idle and wait for more work
-            std::unique_lock<std::mutex> ul(m_work_mutex);
             m_signal_work.wait(ul, [worker_id, this]{
-                bool workAvailable = m_work_available[worker_id-1];
+                bool workAvailable = m_work_available[worker_id];
                 return workAvailable;
             });
-
-			LOG("%d: WORK AVAILABLE", worker_id);
         }
     }
 
     void wait() {
-        std::unique_lock<std::mutex> ul(m_done_mutex);
+        std::unique_lock<std::mutex> ul(m_signal_mutex);
         m_signal_done.wait(ul, [this]{
-
-            LOG("GOT A SIGNAL");
 
             for (auto workAvailable : m_work_available) {
                 if(workAvailable) return false;
             }
-            LOG("RETURN TRUE");
             return true;
         });
     }
